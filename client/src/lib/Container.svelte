a5901da5ee0866cb5072697e4a5621c9987ae7ac
Atsushi <kakuta.atsushi@outward.jp>
2023-02-07 20:46:40 +0900

    indexページのheaderとcardの作成

---
+++ client/src/lib/Container.svelte
@@ -0,0 +1,16 @@
+<script>
+    let toDoItems = [1,2,3,4,5];
+</script>
+{#each toDoItems as todoItem}
+    <div class="row">
+        <div class="col s12 m6">
+            <div class="card">
+                <span class="card-title" style="color: black">Card Title</span>
+                <div class="card-content">
+                    <p style="color: black">I am a very simple card. I am good at containing small bits of information. I am
+                        convenient because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+{/each}
